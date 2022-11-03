403ead7d648b0ea3d87654b61905380d60f49d99
Alyssa Sitto <alyssasitto1@gmail.com>
2022-11-02 18:14:31 -0700

    Replace x icon image

--- src/pages/Recipes/RecipesPage.js
+++ src/pages/Recipes/RecipesPage.js
@@ -115,7 +115,22 @@ function RecipePage() {
 				<div className="filter-box">
 					<div onClick={showFilterOptions} className="filter-btn">
 						<p className="filter">Filter</p>
-						<img src="images/plus.png" className={"plus-icon " + rotate}></img>
+						<div className="plus-icon">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M12 4.5v15m7.5-7.5h-15"
+								/>
+							</svg>
+						</div>
 					</div>
 					<div className={"inner-box " + filterOptions}>
 						<h2>Cuisine</h2>
